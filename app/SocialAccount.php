33bb1929c1b1a85cc80d48f95bd6844dc195d08c
zolowin <57177546+zolowin@users.noreply.github.com>
2020-03-12 11:39:57 +0700

    gg

--- app/SocialAccount.php
+++ app/SocialAccount.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class SocialAccount extends Model
 {
     protected $fillable = ['user_id', 'provider_user_id', 'provider'];
+    protected $primaryKey = 'provider_user_id';
 
     public function user()
     {
--- database/migrations/2020_03_12_025308_create_social_accounts_table.php
+++ database/migrations/2020_03_12_025308_create_social_accounts_table.php
@@ -14,9 +14,9 @@ class CreateSocialAccountsTable extends Migration
     public function up()
     {
         Schema::create('social_accounts', function (Blueprint $table) {
-            $table->integer('user_id');
             $table->string('provider_user_id');
             $table->string('provider');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
